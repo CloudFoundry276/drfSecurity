af73a352b5b8c9186d31a53841f5348a577e0329
CloudFoundry276 <cloudfoundry276@gmail.com>
2022-03-15 22:01:54 +0530

    global security is done

--- securityApp/views.py
+++ securityApp/views.py
@@ -1,16 +1,12 @@
 from .models import Author, Book
 from securityApp.serializers import AuthorSerializer, BookSerializer
 from rest_framework import generics
-from rest_framework.authentication import BasicAuthentication
-from rest_framework.permissions import IsAuthenticated, DjangoModelPermissions
 
 
 # Create your views here.
 class AuthorList(generics.ListCreateAPIView):
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
-    authentication_classes = [BasicAuthentication]
-    permission_classes = [IsAuthenticated, DjangoModelPermissions]
 
 class AuthorDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Author.objects.all()
